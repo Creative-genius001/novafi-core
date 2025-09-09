ad9e37cca7f73ffd8df3c948b18a12995a825b89
unknown <ovieighosuakpo@gmail.com>
2025-09-09 13:39:34 +0100

    fixed and cleaned up transaction module

--- src/modules/transaction/transaction.module.ts
+++ src/modules/transaction/transaction.module.ts
@@ -5,10 +5,9 @@ import { TransactionController } from './transaction.controller';
 import { PrismaModule } from 'src/infrastructure/prisma/prisma.module';
 import { LoggerModule } from 'src/common/logger/logger.module';
 import { TransactionRepository } from 'src/modules/transaction/repo/transaction.repository';
-import { FlutterwaveModule } from 'src/flutterwave/flutterwave.module';
 
 @Module({
-  imports: [PrismaModule,LoggerModule, FlutterwaveModule],
+  imports: [PrismaModule,LoggerModule],
   providers: [TransactionService, TransactionRepository],
   controllers: [TransactionController]
 })
--- src/modules/transaction/transaction.service.ts
+++ src/modules/transaction/transaction.service.ts
@@ -1,16 +1,12 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { AppLogger } from 'src/common/logger/logger.service';
-import { FlutterwaveService } from 'src/flutterwave/flutterwave.service';
-import { BillCategoryCode, CreateBillPayment, RetrieveAccountName } from 'src/flutterwave/types/flutterwave';
 import { TransactionRepository } from 'src/modules/transaction/repo/transaction.repository';
-import { CreateBillPaymentDto, RetrieveAccountNameDto } from './dto/transaction.dto';
 
 @Injectable()
 export class TransactionService {
     constructor(
         private readonly repo: TransactionRepository,
-        private readonly flutterwave: FlutterwaveService,
         private readonly logger: AppLogger
     ){}
 
@@ -18,7 +14,7 @@ export class TransactionService {
         const transactions =  await this.repo.getAllTransactions(userId)
 
         if (!transactions){
-            this.logger.error('WALLET_RETRIEVAL', { userId } )
+            this.logger.error('ALL_TRANSACTIONS_RETRIEVAL', { userId } )
             throw new InternalServerErrorException('Could not retrive all transactions')
         }
 
@@ -29,40 +25,11 @@ export class TransactionService {
         const transaction =  await this.repo.getSingleTransaction(transactionId)
 
         if (!transaction){
-            this.logger.error('WALLET_BALANCE_RETRIEVAL', { transactionId } )
+            this.logger.error('SINGLE_TRANSACTION_RETRIEVAL', { transactionId } )
             throw new InternalServerErrorException('Could not retrive transaction')
         }
 
         return transaction;
     }
-
-    async getBanks(){
-        return this.flutterwave.getAllBanks()
-    }
-
-    async getBillerInformation(categoryCode: BillCategoryCode){
-        return this.flutterwave.getBillerInformation(categoryCode)
-    }
-
-    async createBillPayment(payload: CreateBillPaymentDto){
-        const data: CreateBillPayment = {
-            item_code: payload.itemCode,
-            customer_id: payload.customerId,
-            callback_url:payload.callbackUrl,
-            biller_code: payload.billerCode,
-            country: 'NGN',
-            amount: payload.amount,
-            reference: payload.reference
-        }
-        return this.flutterwave.createBillPayment(data)
-    }
-
-    async retrieveAccountName(payload: RetrieveAccountNameDto){
-        const data: RetrieveAccountName = {
-            account_bank: payload.accountBank,
-            account_number: payload.accountNumber
-        }
-
-        return this.flutterwave.retrieveAccountName(data)
-    }
+    
 }
